15d9b73609428eda4729015e93a7bdd127c60324
JunyeolYu <ryuwhale95@gmail.com>
2023-10-19 05:09:23 +0000

    test: add test script

---
+++ examples/pytorch/llama/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+start=$(date +%s.%N)
+
+#python main.py    --model hf-causal-experimental     --model_args pretrained=/home/llama-30b/,use_accelerate=True     --tasks hellaswag     --device cuda     --batch_size 16
+mpirun -n 4 --allow-run-as-root python llama_example.py --output_len 1 --pipeline_para_size 4 --ckpt_path /model/llama/4-gpu/ --tokenizer_path /model/llama-30b/ --lib_path /root/FasterTransformer_/build/lib/libth_transformer.so
+
+finish=$(date +%s.%N)
+time=$( echo "$finish - $start" | bc -l )
+echo 'time:' $time
