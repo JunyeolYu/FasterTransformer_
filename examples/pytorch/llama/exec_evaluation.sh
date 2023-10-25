a33fed295feeed2865472dd6352fef4c7d5b0947
Junyeol Yu <ryuwhale95@gmail.com>
2023-10-25 11:00:12 +0900

    Update exec_evaluation.sh

--- examples/pytorch/llama/exec_evaluation.sh
+++ examples/pytorch/llama/exec_evaluation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+SRC="/workspace/LlamaRanch/src/FasterTransformer"
+CKPT="/model/llama/4-gpu"
+TOK="/model/llama-30b"
+
 start=$(date +%s.%N)
 
-python preprocessing.py --tokenizer_path /model/llama-30b/ &
-mpirun -n 4 --allow-run-as-root python main.py --output_len 1 --pipeline_para_size 4 --ckpt_path /model/llama/4-gpu/ --tokenizer_path /model/llama-30b/ --lib_path /root/FasterTransformer_/build/lib/libth_transformer.so
-
-#python preprocessing.py --tokenizer_path $TOKENIZER_PATH &
-#mpirun -n 4 --allow-run-as-root python main.py --output_len 1 --pipeline_para_size 4 --ckpt_path $CKPT_PATH --tokenizer_path $TOKENIZER_PATH --lib_path $LIB_PATH
+python preprocessing.py --tokenizer_path $TOK &
+mpirun -n 4 --allow-run-as-root python main.py --output_len 1 --pipeline_para_size 4 --ckpt_path $CKPT --tokenizer_path $TOK --lib_path $SRC/build/lib/libth_transformer.so
 
 finish=$(date +%s.%N)
 time=$( echo "$finish - $start" | bc -l )
